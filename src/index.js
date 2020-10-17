87b40f81218e6e93a1b815b59e0b04dcbde46487
jjanmo <jjanmo07@hanmail.net>
2020-10-18 01:57:02 +0900

    Add counter of vanillaJS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import './index.css';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+(function () {
+    const $result = document.querySelector('.result');
+    const $plusButton = document.querySelector('.plus');
+    const $minusButton = document.querySelector('.minus');
+    let count = Number($result.textContent);
+
+    const updateCount = () => {
+        $result.textContent = count;
+    };
+
+    const handlePlus = () => {
+        count = count + 1;
+        updateCount();
+    };
+
+    const handleMinus = () => {
+        count = count - 1;
+        updateCount();
+    };
+
+    $plusButton.addEventListener('click', handlePlus);
+    $minusButton.addEventListener('click', handleMinus);
+})();
