f864d352a3a72db8bb8a78a8fa205853d7a67dbd
jjanmo <jjanmo07@hanmail.net>
2021-03-09 23:39:41 +0900

    Reset ghpages

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,65 +1,65 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { actionCreator } from '../store';
 
 const Todo = styled.li`
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: center;
-    border-radius: 5px;
-    padding: 0.5rem;
-    transition: 0.3s;
-    &:hover {
-        background-color: #eee;
-    }
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 5px;
+  padding: 0.5rem;
+  transition: 0.3s;
+  &:hover {
+    background-color: #eee;
+  }
 `;
 const SLink = styled(Link)`
-    width: 80%;
-    display: inline-block;
+  width: 80%;
+  display: inline-block;
 `;
 const Title = styled.span`
-    margin: 0 0.2rem;
-    padding: 0.3rem;
-    font-size: 1.2rem;
-    text-decoration: ${(props) => (props.status ? 'line-through' : 'none')};
+  margin: 0 0.2rem;
+  padding: 0.3rem;
+  font-size: 1.2rem;
+  text-decoration: ${(props) => (props.status ? 'line-through' : 'none')};
 `;
 const Button = styled.button`
-    all: unset;
-    cursor: pointer;
-    width: 10%;
-    text-align: center;
-    text-transform: uppercase;
-    padding: 0.2rem 0;
-    border-radius: 5px;
-    color: #fff;
-    background-color: #27ae60;
+  all: unset;
+  cursor: pointer;
+  width: 10%;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 0.2rem 0;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #27ae60;
 `;
 
 function Item({ dispatchEditStatus, dispatchDeleteTodo, ...rest }) {
-    const onChange = () => {
-        dispatchEditStatus();
-    };
+  const onChange = () => {
+    dispatchEditStatus();
+  };
 
-    return (
-        <Todo>
-            <input type="checkbox" name="checkbox" onChange={onChange} checked={rest.status} />
-            <SLink to={`/${rest.id}`}>
-                <Title status={rest.status}>{rest.todo.title}</Title>
-            </SLink>
-            <Button onClick={dispatchDeleteTodo}>Del</Button>
-        </Todo>
-    );
+  return (
+    <Todo>
+      <input type="checkbox" name="checkbox" onChange={onChange} checked={rest.status} />
+      <SLink to={`/${rest.id}`}>
+        <Title status={rest.status}>{rest.todo.title}</Title>
+      </SLink>
+      <Button onClick={dispatchDeleteTodo}>Del</Button>
+    </Todo>
+  );
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
-    return {
-        dispatchDeleteTodo: () => dispatch(actionCreator.deleteTodo(ownProps.id)),
-        dispatchEditStatus: () => dispatch(actionCreator.editStatus(ownProps.id)),
-    };
+  return {
+    dispatchDeleteTodo: () => dispatch(actionCreator.deleteTodo(ownProps.id)),
+    dispatchEditStatus: () => dispatch(actionCreator.editStatus(ownProps.id)),
+  };
 }
 
 export default connect(null, mapDispatchToProps)(Item);
